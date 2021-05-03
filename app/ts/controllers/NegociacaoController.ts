a49dae5b14136de94e76629b7dce8bd1f3bb56f7
Tatiana Viana <tatisabinoviana@gmail.com>
2021-05-03 09:39:43 -0300

    Atribuido tipos para adiciona e paraArray

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,24 +1,31 @@
 class NegociacaoController {
 
-    private _inputData: Element;
-    private _inputQuantidade: Element;
-    private _inputValor: Element;
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
     }
     adiciona(event: Event) {
 
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            this._inputData.value, 
-            this._inputQuantidade.value,
-            this._inputValor.value
+            new Date(this._inputData.value.replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoes.paraArray().forEach(negociacao =>  {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        })
     }
 }
