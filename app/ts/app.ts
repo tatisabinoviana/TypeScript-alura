e26a2b520d48da2b376826215d13b73d6b8de771
tatisabinoviana <tatisabinoviana@gmail.com>
2021-05-12 13:33:12 -0300

    Adicinado um servidor local - lite-server

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,4 @@
+import {NegociacaoController} from './controllers/NegociacaoController';
+
 const controller = new NegociacaoController();
-// usando JQuery!
 $('.form').submit(controller.adiciona.bind(controller));
