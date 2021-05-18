80646c069f8ca814e09358d208b53ba0ff64b9d1
tatisabinoviana <tatisabinoviana@gmail.com>
2021-05-18 10:00:56 -0300

    Adicinado Enuns ao codigo

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -23,8 +23,15 @@ export class NegociacaoController {
 
         event.preventDefault();
 
+        let data= new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)){
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return;
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')), 
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -33,4 +40,20 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso!');
     }
+
+    private _ehDiaUtil(data: Date){
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo,
+    Segunda,
+    Terça,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
